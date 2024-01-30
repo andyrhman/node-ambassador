8dde3ea6be6d9da93d5bb294feedfefa58b2cca1
andyrhman <andyrahman989@gmail.com>
2024-01-30 21:14:55 +0800

    Added formatted rankings in the link.controller.ts

--- src/controller/link.controller.ts
+++ src/controller/link.controller.ts
@@ -76,9 +76,34 @@ export const Stats = async (req: Request, res: Response) => {
 
 export const Rankings = async (req: Request, res: Response) => {
     try {
-        const result = await client.sendCommand(['ZREVRANGEBYSCORE', 'rankings', '+inf', '-inf', 'WITHSCORES']);
+        const result: string[] = await client.sendCommand(['ZREVRANGEBYSCORE', 'rankings', '+inf', '-inf', 'WITHSCORES']);
 
-        res.send(result);
+        const rankings = {};
+        for (let i = 0; i < result.length; i += 2) {
+            const name = result[i];
+            const score = parseInt(result[i + 1]);
+            rankings[name] = score;
+        }
+
+        res.send(rankings);
+
+        /*
+        * BUG VERSION
+        ?    let name;
+        
+        ?    res.send(result.reduce((o, r) => {
+        ?        if (isNaN(parseInt(r))) {
+        ?            name = r;
+        ?            return o;
+        ?        } else {
+        ?            return {
+        ?                ...o,
+        ?                [name]: parseInt(r)
+        ?            };
+        ?        }
+        ?    }, {}));
+        
+        */
     } catch (error) {
         logger.error(error);
         return res.status(400).send({ message: "Invalid Request" })
